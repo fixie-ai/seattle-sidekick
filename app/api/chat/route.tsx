20a29bc3ab3c0f1ebfd385c5545518d72e2a3636
Nick Heiner <nick.heiner+github@gmail.com>
2023-07-26 15:15:19 -0700

    Use ai-jsx 0.6.0

--- app/api/chat/route.tsx
+++ app/api/chat/route.tsx
@@ -4,14 +4,11 @@ import { toTextStream } from 'ai-jsx/stream'
 import {
   ChatCompletion,
   ConversationHistory,
-  FunctionCall,
-  FunctionResponse,
   SystemMessage,
   UserMessage
 } from 'ai-jsx/core/completion'
 import { Prompt } from 'ai-jsx/batteries/prompts'
-import { Tool, UseToolsProps as UpstreamUseToolsProps } from 'ai-jsx/batteries/use-tools';
-import { NaturalLanguageRouter, Route } from 'ai-jsx/batteries/natural-language-router'
+import { Tool, UseTools } from 'ai-jsx/batteries/use-tools';
 import { OpenAI } from 'ai-jsx/lib/openai'
 import { StreamingTextResponse } from 'ai'
 import { PropsOfComponent } from 'ai-jsx'
@@ -20,78 +17,8 @@ import url from 'node:url';
 import querystring from 'node:querystring';
 import _ from 'lodash'
 import * as AI from 'ai-jsx'
-import {LogImplementation, LogLevel, PinoLogger} from 'ai-jsx/core/log'
+import {PinoLogger} from 'ai-jsx/core/log'
 import { pino } from 'pino';
-import { AIJSXError, ErrorCode } from 'ai-jsx/core/errors';
-
-interface UseToolsProps extends Omit<UpstreamUseToolsProps, 'query'> {
-  messages: Messages;
-}
-
-async function* UseTools(props: UseToolsProps, { render }: AI.RenderContext) {
-  const messages = [
-    <SystemMessage>You are a smart agent that may use functions to answer a user question.</SystemMessage>,
-  ];
-  if (props.fallback) {
-    messages.push(
-      <SystemMessage>Here{"'"}s the fallback strategy/message if something failed: {props.fallback}</SystemMessage>
-    );
-  }
-  messages.push(<ConversationHistory messages={props.messages} />);
-
-  do {
-    const modelResponse = <ChatCompletion functionDefinitions={props.tools}>{messages}</ChatCompletion>;
-
-    const renderResult = yield* render(modelResponse, { stop: (el) => el.tag == FunctionCall });
-
-    const stringResponses: String[] = [];
-    let functionCallElement: AI.Element<any> | null = null;
-
-    for (const element of renderResult) {
-      if (typeof element === 'string') {
-        // Model has generated a string response. Record it.
-        stringResponses.push(element);
-      } else if (AI.isElement(element)) {
-        // Model has generated a function call.
-        if (functionCallElement) {
-          throw new AIJSXError(
-            `ChatCompletion returned 2 function calls at the same time ${renderResult.join(', ')}`,
-            ErrorCode.ModelOutputCouldNotBeParsedForTool,
-            'runtime'
-          );
-        }
-        functionCallElement = element;
-      } else {
-        throw new AIJSXError(
-          `Unexpected result from render ${renderResult.join(', ')}`,
-          ErrorCode.ModelOutputCouldNotBeParsedForTool,
-          'runtime'
-        );
-      }
-    }
-
-    if (functionCallElement) {
-      messages.push(functionCallElement);
-      yield functionCallElement;
-      // Call the selected function and append the result to the messages.
-      let response;
-      try {
-        const callable = props.tools[functionCallElement.props.name].func;
-        response = await callable(functionCallElement.props.args);
-      } catch (e: any) {
-        response = `Function called failed with error: ${e.message}.`;
-      } finally {
-        const functionResponse = <FunctionResponse name={functionCallElement.props.name}>{response}</FunctionResponse>;
-        yield functionResponse;
-        messages.push(functionResponse);
-      }
-    } else {
-      // Model did not generate any function call. Return the string responses.
-      return stringResponses.join('');
-    }
-  } while (true);
-}
-
 
 const pinoStdoutLogger = pino({
   name: 'ai-jsx',
@@ -100,7 +27,7 @@ const pinoStdoutLogger = pino({
 
 type Messages = PropsOfComponent<typeof ConversationHistory>['messages'];
 
-function App({ messages }: { messages: Messages }, {logger}: AI.ComponentContext) {
+function App({ messages }: { messages: Messages }, {logger, render}: AI.ComponentContext) {
 
   const defaultSeattleCoordinates = '47.6062,-122.3321';
   const tools: Record<string, Tool> = {
@@ -179,33 +106,56 @@ function App({ messages }: { messages: Messages }, {logger}: AI.ComponentContext
       func: async ({ origin, destination }) => {
         const googleMapsApiUrl = new url.URL('https://maps.googleapis.com/maps/api/directions/json');
         const params = {
-            destination,
-            origin,
+            destination: ensurePlaceIsDescriptiveEnough(destination),
+            origin: ensurePlaceIsDescriptiveEnough(origin),
             key: process.env.GOOGLE_MAPS_API_KEY
         };
+
+        /**
+         * If you pass `ballard` to the Google Maps API, it fails. You need to pass `ballard, seattle` or `ballard, wa`.
+         * We tell the AI this, but it doesn't always comply.
+         */
+        function ensurePlaceIsDescriptiveEnough(place: string) {
+          /**
+           * I think this will work in most cases. If the AI has already appended `, WA` to the string, Google Maps 
+           * tolerates having multiple `, WA` suffixes.
+           * 
+           * This will fail if the user asks for directions out of state.
+           */
+          return `${place}, WA`;
+
+          /**
+           * If the above simple approach didn't work, we could so something like:
+           */
+
+          // return render(<OpenAI chatModel='gpt-3.5-turbo'>
+          //   <ChatCompletion>
+          //     <SystemMessage>The user will give you a place name, like {'"'}Capitol Hill{'"'}. If the place name is too specific, fix it by adding the city and state name to the end. For instance, {'"'}Capitol Hill{'"'} would become {'"'}Capitol Hill, Seattle, WA{'"'}.</SystemMessage>
+          //     <UserMessage>{place}</UserMessage>
+          //   </ChatCompletion>
+          // </OpenAI>)
+        }
         
         googleMapsApiUrl.search = querystring.stringify(params);
         try {
           // @ts-expect-error got types are wrong
           const responseBody = await got(googleMapsApiUrl.toString()).json();
-          logger.info({responseBody, destination, origin}, 'Got response from google maps directions API') 
+          logger.info({responseBody, ..._.omit(params, 'key')}, 'Got response from google maps directions API') 
           return JSON.stringify(responseBody);
         } catch (e) {
-          logger.error({e, destination, origin}, 'Got error calling google maps directions API')
+          logger.error({e, ..._.omit(params, 'key')}, 'Got error calling google maps directions API')
           throw e;
         }
       }
     }
   }
 
-  const latestMessage = _.last(messages)!;
-
   return (
     <OpenAI chatModel='gpt-4'>
-      <ChatCompletion>
+      <UseTools tools={tools} showSteps fallback=''>
         <SystemMessage>
           <Prompt hhh persona="expert travel planner" />
-          You help users with plan activities in Seattle. You can look for locations and find directions. If a user asks for anything not related to that, tell them you can{"'"}t help.
+          You help users with plan activities in Seattle. You can look for locations and find directions. If a user asks for anything not related to that, tell them you cannott help.
 
           If the user asks for location information and directions, you will be given live API calls in subsequent systems messages. You should respond to the user{"'"}s request using the results of those API calls. If those API calls errored out, tell the user there was an error making the request. Do not tell them you will try again.
           
@@ -213,26 +163,9 @@ function App({ messages }: { messages: Messages }, {logger}: AI.ComponentContext
           Do not attempt to answer using your latent knowledge.
           
           Respond concisely, using markdown formatting to make your response more readable and structured.
-
-          Do not say {"'"}now retrieving data{"'"} or {"'"}I{"'"}ll query that for you{"'"} or anything like that. All the queries are already done.
-
-          Do not say {"'"}I can help with that{"'"} if you are not actually giving the full answer.
         </SystemMessage>
-        <NaturalLanguageRouter query={latestMessage.content!}>
-          <Route when="to respond to the user's request, it would be helpful to get directions">
-            <SystemMessage>
-            API results of directions the user asked for: <UseTools tools={tools} fallback='Tell the user there was an error making the request.' messages={messages} />
-            </SystemMessage>
-          </Route>
-          <Route when="to respond to the user's request, it would be helpful search for locations">
-            <SystemMessage>
-            Do not use your own knowledge about places. Instead, use the results of this live API call: <UseTools tools={tools} fallback='Tell the user there was an error making the request.' messages={messages} />
-            </SystemMessage>
-          </Route>
-          <Route unmatched><></></Route>
-        </NaturalLanguageRouter>
         <ConversationHistory messages={messages} />
-      </ChatCompletion>
+      </UseTools>
     </OpenAI>
   )
 }
